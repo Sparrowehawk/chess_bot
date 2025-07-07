5732ee8693b92f0c8d8a62ebb680bd872ea59ef7
Hamid Jamal <hamidlikestocode101@gmail.com>
2025-07-07 18:02:11 +0100

    Bishop + tests

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,14 @@ pub use bitboard::Bitboard;
 fn main() {
     println!("Hello");
     let mut board = Bitboard::new();
-    let success = board.move_pawn(12, 28, true);
+    let success = board.move_pawn(51, 43, false);
+    if success {
+        Bitboard::print_board(&board);
+    } else {
+        println!("Illegal move");
+    }
+
+    let success = board.move_bishop(58, 40, false);
     if success {
         Bitboard::print_board(&board);
     } else {
